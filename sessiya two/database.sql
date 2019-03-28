e7a1149a2eb322435115798bb1ca3e8bd2f85eb3
ya <ya>
2019-03-28 15:54:59 +0300

    UPDATE

--- sessiya two/database.sql
+++ sessiya two/database.sql
@@ -2,24 +2,27 @@ CREATE DATABASE LAB2;
 USE LAB2;
 CREATE TABLE `Книги` (
     `ISBN` VARCHAR(32) NOT NULL PRIMARY KEY,
-    `Порядковый № автора` INT NOT NULL,
+    `Порядковый № автора` INT(11) NOT NULL,
     `Название книги` VARCHAR(25) NOT NULL,
-    `Порядковый № издательства` INT NOT NULL,
+    `Порядковый № издательства` INT(11) NOT NULL,
     `Год издания` INT NOT NULL,
-    `Цена` INT NOT NULL 
+    `Цена` INT NOT NULL,
+	FOREIGN KEY (`Порядковый № автора`) REFERENCES `Авторы` (`Порядковый № автора`) ON UPDATE CASCADE,
+	FOREIGN KEY (`Порядковый № издательства`) REFERENCES `Издательства`(`Порядковый № издательства`) ON UPDATE CASCADE
 ) ENGINE=InnoDB;
+
 CREATE TABLE `Авторы` (
-    `Порядковый № автора` INT NOT NULL PRIMARY KEY, 
+    `Порядковый № автора` INT(11) NOT NULL, 
     `ФИО автора` VARCHAR(60) NOT NULL,
     `Пол` VARCHAR(1) NOT NULL, 
-    `Дата рождения` INT NOT NULL,
-    FOREIGN KEY (`Порядковый № автора`) REFERENCES `Книги` (`Порядковый № автора`) ON UPDATE CASCADE
-) ENGINE=InnoDB;
+    `Дата рождения` CHAR(14) NOT NULL,
+	PRIMARY KEY(`Порядковый № автора`)
+);
+
 CREATE TABLE `Издательства` (
-    `Порядковый № издательства` INT NOT NULL PRIMARY KEY, 
+    `Порядковый № издательства` INT(11) NOT NULL PRIMARY KEY, 
     `Название издательства` VARCHAR(30) NOT NULL,
-    `Город` VARCHAR(15) NOT NULL,
-    FOREIGN KEY (`Порядковый № издательства`) REFERENCES `Книги`(`Порядковый № издательства`) ON UPDATE CASCADE
+    `Город` VARCHAR(15) NOT NULL
 ) ENGINE=InnoDB;
 
 INSERT INTO `Книги` (`ISBN`,`Порядковый № автора`, `Название книги`, `Порядковый № издательства`, `Год издания`, `Цена`) VALUES 
@@ -37,4 +40,13 @@ INSERT INTO `Издательства` (`Порядковый № издател
     (101, 'Питер', 'Петербург'),
     (102, 'Лори', 'Москва'),
     (103, 'БХВ-Петербург', 'Петербург');
+	
+CREATE USER 'administrator'@'localhost' IDENTIFIED BY '123';
+CREATE USER 'director'@'localhost' IDENTIFIED BY '123456';
+CREATE USER 'visitor'@'localhost' IDENTIFIED BY '';
 
+GRANT ALL PRIVILEGES ON * . * TO 'administrator'@'localhost';
+
+GRANT ALL PRIVILEGES ON * . * TO 'worker'@'localhost';
+
+REVOKE 
